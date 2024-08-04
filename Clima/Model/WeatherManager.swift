78ac026b09082ad0e315bc759853f9711ec016d3
Shoiab A. W. Mohammed <msaw9246@gmail.com>
2024-08-03 23:54:40 -0400

    Update WeatherManager.swift

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -1,10 +1,7 @@
 //
 //  WeatherManager.swift
 //  Clima
-//
-//  Created by Angela Yu on 03/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
+
 
 import Foundation
 import CoreLocation
